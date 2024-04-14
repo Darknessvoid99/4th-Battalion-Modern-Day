8cc87af8924878e637c25d74b28363e97b885c89
Darknessvoid99 <63525965+Darknessvoid99@users.noreply.github.com>
2024-04-14 02:59:22 -0500

    Separated custom weapons from main weapon pbo
    - Removed custom weapons from darkmod_4thmd_wep
    - Added 4th_custom_wep pbo with custom weapons

---
+++ 4th_custom_wep/config.cpp
@@ -0,0 +1,62 @@
+class CfgPatches {
+    class 4th_custom_wep
+	{
+        weapons[] = 
+        {
+            "4th_w_m_86070_dmr2_nut",
+            "4th_w_m_86070_dmr2_spk"
+        };
+        units[] = {};
+        requiredAddons[] = 
+            {   
+            "A3_Weapons_F_Exp"
+            };
+
+        author = "Darknessvoid99";
+        version = "0.1.0.2402120109";
+    };
+};
+
+#define DEBUG 1 // 1 = OFF, 2 = ON
+
+class CfgWeapons
+{
+    class srifle_DMR_02_F;
+    class 4th_w_m_86070_dmr2_nut : srifle_DMR_02_F
+    {
+        baseweapon="4thmd_w_m_86070_dmr2_nut";
+
+        author="Frosty & Mac";
+        displayname="4Bt. Nutters' Good News";
+
+        hiddenSelectionsTextures[]=
+		{
+			"4th_custom_wep\data\dmr2\nutters\dmr2_01_nut_CO.paa",
+			"4th_custom_wep\data\dmr2\nutters\dmr2_02_nut_CO.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"4th_custom_wep\data\dmr2\nutters\dmr2_01_nut.rvmat",
+			"a3\weapons_f_mark\LongRangeRifles\DMR_02\Data\DMR_02_02.rvmat"
+		};
+    };
+
+    class 4th_w_m_86070_dmr2_spk : srifle_DMR_02_F
+    {
+        baseweapon="4thmd_w_m_86070_dmr2_spk";
+
+        author="Frosty & Mac";
+        displayname="4Bt. Spooks' Heather";
+
+        hiddenSelectionsTextures[]=
+		{
+			"4th_custom_wep\data\dmr2\spooks\dmr2_01_spk_CO.paa",
+			"4th_custom_wep\data\dmr2\spooks\dmr2_02_spk_CO.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"4th_custom_wep\data\dmr2\spooks\dmr2_01_spk.rvmat",
+			"a3\weapons_f_mark\LongRangeRifles\DMR_02\Data\DMR_02_02.rvmat"
+		};
+    };
+};
--- darkmod_4thmd_wep/config.cpp
+++ darkmod_4thmd_wep/config.cpp
@@ -3,13 +3,13 @@ class CfgPatches {
 	{
         weapons[] = 
         {
-            "4thmd_u_unif1_ar1",
-            "4thmd_u_unif1_ar1_ss"
+            ""
         };
         units[] = {};
         requiredAddons[] = 
             {   
-            "A3_Weapons_F_Exp"
+                "A3_Weapons_F_Exp",
+                "4th_custom_wep"
             };
 
         author = "Darknessvoid99";
@@ -22,41 +22,5 @@ class CfgPatches {
 class CfgWeapons
 {
     class srifle_DMR_02_F;
-    class 4thmd_w_m_86070_dmr2_nut : srifle_DMR_02_F
-    {
-        baseweapon="4thmd_w_m_86070_dmr2_nut";
-
-        author="Frosty & Mac";
-        displayname="4Bt. Nutters' Good News";
-
-        hiddenSelectionsTextures[]=
-		{
-			"darkmod_4thmd_wep\data\dmr2\nutters\dmr2_01_nut_CO.paa",
-			"darkmod_4thmd_wep\data\dmr2\nutters\dmr2_02_nut_CO.paa"
-		};
-		hiddenSelectionsMaterials[]=
-		{
-			"darkmod_4thmd_wep\data\dmr2\nutters\dmr2_01_nut.rvmat",
-			"a3\weapons_f_mark\LongRangeRifles\DMR_02\Data\DMR_02_02.rvmat"
-		};
-    };
-
-    class 4thmd_w_m_86070_dmr2_spk : srifle_DMR_02_F
-    {
-        baseweapon="4thmd_w_m_86070_dmr2_spk";
-
-        author="Frosty & Mac";
-        displayname="4Bt. Spooks' Heather";
-
-        hiddenSelectionsTextures[]=
-		{
-			"darkmod_4thmd_wep\data\dmr2\spooks\dmr2_01_spk_CO.paa",
-			"darkmod_4thmd_wep\data\dmr2\spooks\dmr2_02_spk_CO.paa"
-		};
-		hiddenSelectionsMaterials[]=
-		{
-			"darkmod_4thmd_wep\data\dmr2\spooks\dmr2_01_spk.rvmat",
-			"a3\weapons_f_mark\LongRangeRifles\DMR_02\Data\DMR_02_02.rvmat"
-		};
-    };
+    
 };
